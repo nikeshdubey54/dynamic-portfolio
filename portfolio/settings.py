a8a4723200e772bc5a2f219b90d2c0ed40b06894
nikeshdubey54 <demo@gmail.com>
2026-08-20 18:14:03 +0530

    Fix SSL redirect loop

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -260,7 +260,7 @@ SECURE_REFERRER_POLICY = 'same-origin'
 
 if not DEBUG:
 
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
 
     SESSION_COOKIE_SECURE = True
 
@@ -308,3 +308,6 @@ SITE_AUTHOR = "Nikesh Dubey"
 # =========================================================
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
